b9841513419a41b33dd2af121d2f53c5a5290d00
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-06-11 00:48:08 +0100

    Base spring Boot

---
+++ src/main/java/com/e_buvette/ebuvette/EbuvetteApplication.java
@@ -0,0 +1,13 @@
+package com.e_buvette.ebuvette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbuvetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbuvetteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e_buvette/ebuvette/EbuvetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_buvette.ebuvette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbuvetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
