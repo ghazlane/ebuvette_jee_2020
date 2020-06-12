c36c44460d87566e42b2da678f4fffcb60aa7608
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-06-12 16:03:12 +0100

    Migration to java 11

--- src/main/java/com/e_buvette/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.e_buvette.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.e_buvette.models.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Integer>{
-
-}
--- src/main/java/com/e_buvette/repository/VendeurRepository.java
+++
@@ -1,8 +0,0 @@
-package com.e_buvette.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.e_buvette.models.Vendeur;
-
-public interface VendeurRepository extends JpaRepository<Vendeur, Integer>{
-
-}
