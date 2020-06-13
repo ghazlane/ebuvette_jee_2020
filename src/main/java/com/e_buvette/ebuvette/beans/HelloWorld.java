bce593ca2cfd3def716c4998cd1bdcf72efd43fe
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-06-13 18:03:49 +0100

    Architechture JSF

---
+++ src/main/java/com/e_buvette/ebuvette/beans/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.e_buvette.ebuvette.beans;
+
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.e_buvette.ebuvette.services.ClientServices;
+
+@ManagedBean(name = "helloworld", eager = true)
+public class HelloWorld {
+	// injection de notre repository
+	@Autowired
+	private ClientServices clientServices;
+
+	public HelloWorld() {
+		System.out.println("Hello World !!");
+	}
+
+	public String getMessage() {
+		return "Hello World";
+	}
+
+}
---
+++ src/main/java/com/e_buvette/ebuvette/services/ClientServices.java
@@ -0,0 +1,12 @@
+package com.e_buvette.ebuvette.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.e_buvette.ebuvette.repository.ClientRepository;
+
+public class ClientServices {
+	// injection de notre repository
+	@Autowired
+	private ClientRepository clientRepository;
+
+}
