7a55de39fd02db64674f1268be2a028d6722269a
GHAZLANE MOHAMMED <med.ghazlane@gmail.com>
2020-06-16 00:57:21 +0100

    Change name Bean

--- src/main/java/com/e_buvette/ebuvette/beans/ClientBeans.java
+++ src/main/java/com/e_buvette/ebuvette/beans/ClientBeans.java
@@ -2,14 +2,19 @@ package com.e_buvette.ebuvette.beans;
 
 import javax.faces.bean.ManagedBean;
 
-@ManagedBean(name = "client", eager = true)
+import com.e_buvette.ebuvette.models.Client;
+
+@ManagedBean(name = "clientBean", eager = true)
 public class ClientBeans {
 	// injection de notre repository
 //	@Autowired
 //	private ClientServices clientServices;
 
+	private Client client;
+
 	public ClientBeans() {
 		System.out.println("Bean Client Initialisation !!");
+		client = new Client();
 	}
 
 	public String getFormInscription() {
@@ -19,5 +24,13 @@ public class ClientBeans {
 	public String getListClient() {
 		System.out.println("je suis appele");
 		return "listClient";
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
 	}
 }
